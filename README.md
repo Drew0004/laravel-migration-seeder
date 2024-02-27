f0656f7b4b970ea47fb607f4a2083a035511ac0e
Drew0004 <drdrewgraphic@gmail.com>
2024-02-27 16:05:01 +0100

    Stmpati dati in pagina - completata milestone 1

--- README.md
+++ README.md
@@ -28,7 +28,7 @@ Codice Treno
 Numero Carrozze
 In orario
 Cancellato
-È probabile che siano necessarie altre colonne per far funzionare la tabella nel modo corretto ;) ...
+È probabile che siano necessarie altre colonne per far funzionare la tabella nel modo corretto
 Inserite inizialmente i dati tramite PhpMyAdmin.
 Create Model relativo ed un Controller per mostrare nella home page tutti i treni che sono in partenza dalla data odierna.
 Milestone 2
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,11 +3,23 @@
 @section('page-title', 'Home')
 
 @section('main-content')
-<h1>
-    Laravel Start 1
-</h1>
 
-<h2>
-    Ciao {{ $firstName }} {{ $lastName }}
-</h2>
+<div class="container">
+    <h1>
+        Trains
+    </h1>
+    
+    @foreach ($trains as $train)
+        <ul class="my-3">
+            <li>Nome azienda:{{ $train->agency_name }}</li>
+            <li>Stazione di partenza:{{ $train->departure_station }}</li>
+            <li>Stazione di arrivo:{{ $train->arrival_station }}</li>
+            <li>Data partenza:{{ $train->departure_date }}</li>
+            <li>Data arrivo:{{ $train->arrival_date }}</li>
+            <li>Codice treno:{{ $train->train_code }}</li>
+            <li>Numero vagoni:{{ $train->wagons_number }}</li>
+        </ul>
+    @endforeach
+</div>
+
 @endsection
